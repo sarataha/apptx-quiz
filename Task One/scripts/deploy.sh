44dee6830a35cb2201d2f9f4737f151bbc27020e
Sara Taha <st.elzayat@gmail.com>
2018-08-19 12:49:24 +0200

    cicd

---
+++ Task One/scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'Docker Login - BEGIN'
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo 'Docker Login - END'
+
+echo 'Docker Push - BEGIN'
+docker push sarataha/testapp:$TRAVIS_TAG
+echo 'Docker Push - END'
