82384a07ab8a24cc3d724b478b840eb66796ea79
Sara Taha <st.elzayat@gmail.com>
2018-08-19 16:35:43 +0200

    update deploy.sh

--- Task One & Three/scripts/deploy.sh
+++ Task One & Three/scripts/deploy.sh
@@ -7,3 +7,8 @@ echo 'Docker Login - END'
 echo 'Docker Push - BEGIN'
 docker push sarataha/testapp:$TRAVIS_TAG
 echo 'Docker Push - END'
+
+echo 'Docker Run - BEGIN'
+docker pull sarataha/testapp:$TRAVIS_TAG
+docker run -d sarataha/testapp:$TRAVIS_TAG
+echo 'App is now up and running'
